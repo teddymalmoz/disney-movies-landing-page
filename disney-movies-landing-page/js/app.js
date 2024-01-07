3d653836276391a4dd591e123b92f45cf5ba272b
teddymalmoz <155269428+teddymalmoz@users.noreply.github.com>
2024-01-07 21:50:17 +0100

    Add files via upload

---
+++ disney-movies-landing-page/js/app.js
@@ -0,0 +1,7 @@
+
+function toggleMenu(){
+  const toggleMenu = document.querySelector('.toggleMenu');
+  const navigation = document.querySelector('.navigation');
+  toggleMenu.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
